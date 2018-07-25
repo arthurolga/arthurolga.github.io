8a77c0ba349fc5ca563b76e90c8e4cabb85a6b6c
unknown <arthur_olga@hotmail.com>
2018-07-25 02:39:20 -0300

    insper jr 1

---
+++ js/init.js
@@ -0,0 +1,19 @@
+(function($){
+  $(function(){
+
+    $('.sidenav').sidenav();
+    $('.parallax').parallax();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
+
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.fixed-action-btn');
+    var instances = M.FloatingActionButton.init(elems, options);
+  });
+
+  // Or with jQuery
+
+  $(document).ready(function(){
+    $('.fixed-action-btn').floatingActionButton();
+  });
